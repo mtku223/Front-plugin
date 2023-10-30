ea6720457d06a67b2d01fa4065796920278bbdb5
Matt Kustes <mattkustes@gmail.com>
2023-10-30 17:43:03 -0400

    new script.js index and deconetwork.js

---
+++ src/script.js
@@ -0,0 +1,27 @@
+const fetchData = async () => {
+    try {
+        const response = await fetch('https://crookedmonkey.com/api/json/manage_orders/find', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            body: new URLSearchParams({
+                username: 'mattkustes1',
+                password: 'ViRkLTFtWz69Fr',
+                // ...other parameters as needed
+            }),
+        });
+
+        if (!response.ok) {
+            throw new Error(`Error: ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Fetch error:', error);
+    }
+};
+
+fetchData();
+
