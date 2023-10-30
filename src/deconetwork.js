3c97259adddeecb01e2db7e3b05ead5566f33df8
Matt Kustes <mattkustes@gmail.com>
2023-10-30 17:43:29 -0400

    new update with deco api code

---
+++ src/deconetwork.js
@@ -0,0 +1,25 @@
+// deconetwork.js
+
+// Define and export the function in one step
+export const fetchOrderData = async (orderNumber) => {
+    console.log('fetchData called');  // New log statement
+    try {
+      const response = await fetch(`https://crookedmonkey.com/api/json/manage_orders/find`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `username=mattkustes1&password=ViRkLTFtWz69Fr&criteria[order_number]=${orderNumber}`
+      });
+  
+      if (!response.ok) {
+        throw new Error('Failed to fetch order data');
+      }
+  
+      const data = await response.json();
+      console.log('fetch complete', data);  // New log statement
+      console.log(data);  // Log the data to the console for now
+    } catch (error) {
+      console.error('Fetch error:', error);
+    }
+};
--- src/script.js
+++ src/script.js
@@ -1,27 +1,33 @@
-const fetchData = async () => {
-    try {
-        const response = await fetch('https://crookedmonkey.com/api/json/manage_orders/find', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: new URLSearchParams({
-                username: 'mattkustes1',
-                password: 'ViRkLTFtWz69Fr',
-                // ...other parameters as needed
-            }),
-        });
+// const fetchData = async () => {
+//     try {
+//         const response = await fetch('https://crookedmonkey.com/api/json/manage_orders/find', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/x-www-form-urlencoded',
+//             },
+//             body: new URLSearchParams({
+//                 username: 'mattkustes1',
+//                 password: 'ViRkLTFtWz69Fr',
+//                 // ...other parameters as needed
+//             }),
+//         });
 
-        if (!response.ok) {
-            throw new Error(`Error: ${response.statusText}`);
-        }
+//         if (!response.ok) {
+//             throw new Error(`Error: ${response.statusText}`);
+//         }
 
-        const data = await response.json();
-        console.log(data);
-    } catch (error) {
-        console.error('Fetch error:', error);
-    }
-};
+//         const data = await response.json();
+//         console.log(data);
+//     } catch (error) {
+//         console.error('Fetch error:', error);
+//     }
+// };
 
-fetchData();
+// fetchData();
 
+// script.js
+
+import { fetchOrderData } from './deconetwork.js';
+
+// Call the function with an order number
+fetchOrderData('Order-19891');
